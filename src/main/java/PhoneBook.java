9013518a4a10c34c659fac01ba72f174cb27345e
silent27121984 <sergey27121984@gmail.com>
2023-04-25 17:07:39 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    public Map<String, String> contactsList = new HashMap<>();
+
     public int add(String name, String phoneNumber) {
-        return 0;
+        if (!contactsList.containsValue(name)) {
+            contactsList.put(name, phoneNumber);
+        } else {
+            System.out.println("Контакт с таким именем уже существует");
+        }
+        if (name == null)
+            throw new RuntimeException("Имя контакта пустое");
+        return contactsList.size();
     }
 }
